82d3cc23029c627db784ddd2818285f38dce030f
mostafa854 <57110831+mostafa854@users.noreply.github.com>
2019-10-28 20:30:20 +0200

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,32 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "-";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : Number of people received :100::tada:`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(`FunCord`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login("NjM4NDQzNjQ0NDgyNzQ4NDI2.Xbczfg.DXwUS8tCOhSHYh9jy5oa_3ND0yg");
